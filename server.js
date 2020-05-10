133b13aeccfebf64321c8836fe113393129d1f9f
cham_why <53082717+sjufkaus2@users.noreply.github.com>
2020-05-10 20:37:52 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "node_modules")));
+
 app.get("/", (req, res) => {
   res.sendFile("/index.html");
 });
